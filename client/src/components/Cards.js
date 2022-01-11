fbc4939f1d349c20e8ab6d82e697ef24651705fe
Jesus Carrasquel <jesusmcc1106@gmail.com>
2022-01-11 19:03:48 +0100

    remove plan basico

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap'
-import '../assets/styles/home/cards.css'
+import React from "react";
+import { Row, Col } from "reactstrap";
+import "../assets/styles/home/cards.css";
 
 const Cards = () => {
-    return (
-        <div id="cards">
-
-            <Row className="cards">
-                <Col md="12" xs="12" className="card-container mt-4">
-                    
-                        <Col md="4" xs="12" className="badge">
+  return (
+    <div id="cards">
+      <Row className="cards">
+        <Col md="12" xs="12" className="card-container mt-4">
+          {/* <Col md="4" xs="12" className="badge">
                             <a href="#planes">
                                 <div className="front">
                                         <h2>Plan Básico</h2>
@@ -18,44 +16,33 @@ const Cards = () => {
                                         <h4>Cuenta online o en PR</h4>
                                 </div>
                             </a>
-                        </Col>
+                        </Col> */}
 
-                    <Col md="4" xs="12" className="badge">
-                        <a href="#planes">
-                            <div className="front">
-                                
-                                    <h2>Plan Plus</h2>
-                            
-                            </div>
-                            <div className="back">
-                                
-                                    <h4>Cuenta en banco tradicional en US con Zelle Jurídico</h4>
-                                
-                            </div>
-                        </a>
-                    </Col>
+          <Col md="4" xs="12" className="badge">
+            <a href="#planes">
+              <div className="front">
+                <h2>Plan Plus</h2>
+              </div>
+              <div className="back">
+                <h4>Cuenta en banco tradicional en US con Zelle Jurídico</h4>
+              </div>
+            </a>
+          </Col>
 
-                    <Col md="4" xs="12" className="badge">
-                        <a href="#planes">
-                            <div class="front">
-                                    <h2>Plan Corporativo</h2>
-                            </div>
-                            <div className="back">
-                                    <h4>2 cuentas en banco tradicional con Zelle Jurídico</h4>
-                            </div>
-                        </a>
-                    </Col>
-                
-                </Col>
+          <Col md="4" xs="12" className="badge">
+            <a href="#planes">
+              <div class="front">
+                <h2>Plan Corporativo</h2>
+              </div>
+              <div className="back">
+                <h4>2 cuentas en banco tradicional con Zelle Jurídico</h4>
+              </div>
+            </a>
+          </Col>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-
-            </Row>
-
-
-  
-
-        </div>
-    )
-}
-
-export default Cards
+export default Cards;
