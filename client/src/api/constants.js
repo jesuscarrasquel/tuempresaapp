eda33506ac823552afd8cd6290186a3167ba05c2
Jesus Carrasquel <jesusmcc1106@gmail.com>
2022-01-12 21:23:27 +0100

    cambiar constantes apis

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,8 +1,8 @@
-export const url_client = 'http://localhost:3000';
-// export const url_client = 'https://www.tuempresa.us';
+// export const url_client = 'http://localhost:3000';
+export const url_client = "https://www.tuempresa.us";
 
-export const url_server = 'http://localhost/tuempresausapp/server/';
-// export const url_server = 'https://www.tuempresa.us/tuempresausback/';
+// export const url_server = 'http://localhost/tuempresausapp/server/';
+export const url_server = "https://www.tuempresa.us/tuempresausback/";
 
 export const llc = `${url_client}/llc`;
 export const inc = `${url_client}/inc`;
